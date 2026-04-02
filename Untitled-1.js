84c0329100206bfe407da82a35adb0a1d86b5f5a
LeonardoRitter <leonardoritter_@hotmail.com>
2026-04-01 21:45:14 -0300

    tarefa

--- Untitled-1.js
+++ Untitled-1.js
@@ -1,9 +1,14 @@
-for (let i = 10; i>=0 0; i--) {
+for (let i = 10; i>= 0; i--) {
     console.log(i);
 }
 
 
-let numero = 5
+ Oque eu fiz let numero = 5;
 for ( i = 1; i<=10; i++) {
     console.log( {numero}*{i} = {numero * i});
+}
+
+o certo é =
+for (let i = 1; i <= 10; i++) {
+  console.log("5 x " + i + " = " + (5 * i));
 }
