0963e31e8b3721e997c412b3f605e6b0f8814bbc
LeonardoRitter <leonardoritter_@hotmail.com>
2026-04-02 21:01:29 -0300

    tarefa

--- Untitled-1.js
+++ Untitled-1.js
@@ -12,3 +12,29 @@ o certo é =
 for (let i = 1; i <= 10; i++) {
   console.log("5 x " + i + " = " + (5 * i));
 }
+
+
+
+while (let= 2 ) i=0; i>=0; i++; {
+console.log( 2 x + i + = + (5*i) ) 
+}
+
+
+let temperatura = 26;
+let opcao = 3;
+while (opcao != 4){
+    opcao = prompt( 
+        "1-Aumentar a temperatura\n" +
+        "2-Diminuir a temperatura\n" +
+        "3-Mostrar a temperatura atual\n" +
+        "4-Sair tempearatura atual e mensagem de aviso\n" 
+    );
+    if (opcao === 3) {
+        temperatura++;
+        alert("temperatura aumentada para " + temperatura "°c");
+
+    }
+
+}
+
+
